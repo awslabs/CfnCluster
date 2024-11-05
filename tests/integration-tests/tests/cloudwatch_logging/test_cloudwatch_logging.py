2f67ab5e381d805da6d0bd9711da02569880b3f8
Xuanqi He <xuanqihe@amazon.com>
2024-11-05 18:20:04 -0500

    Ensure the instance is in 'running' state before try to ssh it.

--- tests/integration-tests/tests/cloudwatch_logging/test_cloudwatch_logging.py
+++ tests/integration-tests/tests/cloudwatch_logging/test_cloudwatch_logging.py
@@ -220,6 +220,9 @@ class CloudWatchLoggingClusterState:
     def _get_initial_cluster_log_state(self):
         """Get EC2 instances belonging to this cluster. Figure out their roles in the cluster."""
         for instance in cw_logs_utils.get_ec2_instances():
+            # Ensure the instance is in 'running' state
+            if instance.get('State', {}).get('Name') != 'running':
+                continue
             tags = {tag.get("Key"): tag.get("Value") for tag in instance.get("Tags", [])}
             if tags.get("parallelcluster:cluster-name", "") != self.cluster.name:
                 continue
